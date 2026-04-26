ad8275a18e621177e8d03cfc24600aa902e9b0b2
Vineeth Sreepad <vineethsreepad@Vineeths-MacBook-Air.local>
2026-04-26 12:39:07 +0530

    vsreepad/phase-1 catalogservice: commit all the wiring to run catalog service

--- cleanarchitecture/Service/Catalog/Catalog.API/Program.cs
+++ cleanarchitecture/Service/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,45 @@
+using System.Reflection;
+using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
 using Catalog.Infrastructure.Settings;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Register customer Serializers - This will store the data in string and if we dont have this then mongo db will store the data in binary
+// "id": ""
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
 
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add Swagger Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register the mediater 
+var assemblies = new Assembly[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(GetAllBrandsHandler).Assembly
+};
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+
+// Customer Services
+
+builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+builder.Services.AddScoped<ITypeRepository, TypeRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 // Bind strongly typed settings
 builder.Services.Configure<DataBaseSettings>(builder.Configuration.GetSection("DataBaseSettings"));
@@ -22,13 +54,27 @@ builder.Services.AddSingleton<IMongoClient>(sp =>
 
 var app = builder.Build();
 
+// Seed MongoDb on Start Up
+using (var scope = app.Services.CreateScope())
+{
+    var options = scope.ServiceProvider.GetRequiredService<IOptions<DataBaseSettings>>();
+    await DatabaseSeeder.SeedDataAsync(options);
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// Enable Swager
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
